83abb3ffa57914017f08dd3148a1dab98477906e
AnasMouak <anasmouak1111@gmail.com>
2024-03-09 12:44:48 -0800

    Defines the City class

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines the City class."""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """Represents a City."""
+    state_id = ""
+    name = ""
